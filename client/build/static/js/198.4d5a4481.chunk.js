29016dcb528cd0248a8231ebf372a5be6227aff1
Sean-Weller <sweller2018@fau.edu>
2025-04-26 23:07:33 -0400

    netfly

---
+++ client/build/static/js/198.4d5a4481.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.4d5a4481.chunk.js.map
